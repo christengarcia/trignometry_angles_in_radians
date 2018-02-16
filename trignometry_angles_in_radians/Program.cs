b35daaa315b8a729ed499642af1c6bac83eb2c40
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-02-17 07:40:07 +1100

    Initial commit

---
+++ trignometry_angles_in_radians/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * C# Program to Illustrate Trignometry Angles in Radians
+ */
+
+using System;
+
+namespace trignometry_angles_in_radians
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Trignometric Values in Radians : ");
+            Console.WriteLine("sin (pi/3)    = {0}", Math.Sin(Math.PI / 3));
+            Console.WriteLine("cos (pi/3)    = {0}", Math.Cos(Math.PI / 3));
+            Console.WriteLine("tan (pi/3)    = {0}", Math.Tan(Math.PI / 3));
+            Console.WriteLine("arcsin (1/2)  = {0}", Math.Asin(0.5));
+            Console.WriteLine("arccos (1/2)  = {0}", Math.Acos(0.5));
+            Console.WriteLine("arctan (1/2)  = {0}", Math.Atan(0.5));
+            Console.ReadLine();
+        }
+    }
+}
